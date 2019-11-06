041fb09ddfff25fa9e51607664ada884f18e8d9b
PascalUlor <pascalulor@yahoo.com>
2019-11-06 09:28:01 +0100

    plot graph of parents and children

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,9 +1,55 @@
+from util import Stack, Queue
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
     # pass
-    # 1. loop through and find all pairs with start node as child
+    # 1. loop through and find all pairs with start_node/input as child
     # 2. for each pair with start node as child get the parent
         # a. if child has two parent return the least parent
-        # b. if no parent return -1
+        # b. if input has no parent return -1
     # 3. while parents exist repeat steps 1 and 2 for each resulting parent
+    """
+    Graph = {1: {3}, 2: {3}, 3: {6}, 4: {5, 8}, 5: {6,7}, 6: {}, 
+            7: {}, 8: {9}, 9: {}, 10: {1}, 11: {8}}
+    """
+    # Plot Graph
+    gplot = Graph()
+
+    # Plot vertices
+    for parent, child in ancestors:
+        gplot.add_vertex(parent)
+        gplot.add_vertex(child)
+
+    for parent, child in ancestors:
+        gplot.add_edge(parent, child)
+
+    # case where vertex does not exist
+    if not gplot.vertices[starting_node]:
+        return -1
+
+    s = Stack()
+    s.push(starting_node)
     
+
+
+    # print('======',gplot.vertices)
+    # print('+++++++',gplot.vertices[-4])
+    # root = []
+    # for i in range(0, len(ancestors)):
+    #     for j in range(0, len(ancestors[0])):
+    #         if starting_node == ancestors[i][j] and ancestors[i][-1] == ancestors[i][j]:
+    #             fam = list()
+    #             fam.append(ancestors[i])
+    #             print('found', ancestors[i][-1])
+    #             s = Stack()
+    #             visited = set()
+    #             s.push(starting_node)
+    #             while s.size() > 0:
+    #                 vertex = s.pop()
+    #                 if ancestors[i][-1] == vertex and vertex not in visited:
+    #                     visited.add(vertex)
+    #                     s.push(ancestors[i][0])
+    #                 elif ancestors[i][-1] != vertex and vertex in visited:
+    #                     return root.append(vertex)
+    # return root[0]
---
+++ projects/ancestor/graph.py
@@ -0,0 +1,142 @@
+"""
+Simple graph implementation
+"""
+from util import Stack, Queue  # These may come in handy
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        Given that the vertex of each vertice is set
+        so each vertex which is the node and key of the vertices is equated to a set
+        """
+        # pass  # TODO
+        self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            # self.vertices[v2].add(v1)
+        else:
+            print('No vertex at here')
+            raise KeyError("That vertex does not exist")
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        # pass  # TODO
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_vertex)
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                visited.add(v)
+                print('visited node', v)
+                for next_vertex in self.vertices[v]:
+                    q.enqueue(next_vertex)
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        # pass  # TODO
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex)
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                visited.add(v)
+                print('visited node', v)
+                for next_vertex in self.vertices[v]:
+                    s.push(next_vertex)
+
+
+
+
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+    
+    
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print(graph.vertices)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft(1)
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    graph.bft(1)
+
+    '''
+    Valid DFT recursive paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft_recursive(1)
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6))
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    print(graph.dfs(1, 6))
